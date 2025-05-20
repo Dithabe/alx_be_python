ba7cef3fc163ed117b07bc07d87e443c03508258
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-05-20 17:33:33 +0200

    Update finance_calculator.py
    removing float in favour of integer

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -7,9 +7,9 @@ savings = int(income) - int(expenses)
 
 #Annual savings calculator
 interest = 0.05
-projectedSavings = savings * 12 + (savings * 12 * interest)
+projectedSavings = int(savings * 12 + (savings * 12 * interest))
 
 print (f'Your monthly savings are ${savings}')
-print (f'Projected savings after one year, with interest, is: ${projectedSavings}')
+print (f'Projected savings after one year, with interest, is: $int({projectedSavings})')
 
 

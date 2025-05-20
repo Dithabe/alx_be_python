1141cce0e8a45e50d3c5a594fde04a57e58878f8
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-05-20 16:58:53 +0200

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,5 +2,5 @@ principal = 1000 #in dollars
 rate = 0.05 #in percentage
 time = 3 #in years
 
-interest = principal * rate * 3
+interest = principal * rate * time
 print (f"The simple interest is: {interest}")

09c211423b9bf9a5524f887c3e425a475aeedee7
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-05-21 08:38:26 +0200

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
 #Personal Finance Calculator
-income = input("Enter your monthly income: ")
-expenses = input("Enter your monthly expenses: ")
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your monthly expenses: "))
 
 #Do the calculation for monthly savings
-monthly_savings = float(income) - float(expenses)
+monthly_savings = float(monthly_income) - float(monthly_expenses)
 
 #Annual savings calculator
 interest = 0.05

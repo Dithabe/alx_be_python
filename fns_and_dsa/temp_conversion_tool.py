6ec4624fbc16de440ed126b5c4f067c3c90b29fc
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-06-14 00:01:39 +0200

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,46 +1,21 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
+def convert_to_celsius(fahrenheit):
+  global FAHRENHEIT_TO_CELSIUS_FACTOR
+  celcius = (fahrenheit - 32 ) * FAHRENHEIT_TO_CELSIUS_FACTOR
+  print(celcius)
 
-def user_input():
-    temperature = float(input("Enter the temperature to convert: "))
-    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
-    return temperature, unit
+def convert_to_fahrenheit(celsius):
+  global CELSIUS_TO_FAHRENHEIT_FACTOR
+  fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+  print(fahrenheit)
 
-def convert_to_celsius():
-    temperature, unit = user_input()
-    
-    if unit == "F":
-        celcius = (temperature - 32)*FAHRENHEIT_TO_CELSIUS_FACTOR
-        return f"{temperature}°F is {celcius}°C"
-        
-    else:
-        print("The temperature is already in Celcius")
-        
-    
-        
-    
-def convert_to_fahrenheit():
-    temperature, unit = user_input()
-    
-    if unit == "C":
-        fahrenheit = (temperature * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
-        return f"{temperature}°C is {fahrenheit}°F"
-        
-    else:
-        print("The temperature is already in Fahrenheit")
-        
-        
-fahrenheit = convert_to_fahrenheit()
-print(fahrenheit)
-
-celsius = convert_to_celsius()
-print(celsius)
-        
-        
-        
-    
-        
-  
-        
-        
+user = float(input("Enter the temperature to convert: "))
+conversion = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+if conversion == "f":
+  convert_to_celsius(user)
+elif conversion == "c":
+  convert_to_fahrenheit(user)
+else:
+  print("Invalid input! try again")

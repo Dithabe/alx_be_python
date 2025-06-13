9d90222cc1ad37490eae62972a43894beef5a9a5
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-06-13 23:50:56 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,24 +1,12 @@
 def perform_operation(num1, num2, operation):
-    
-    num1 = float(num1)
-    num2 = float(num2)
-    operation = str(operation)
-    
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "devide":
-            
-            if num2 != 0:
-                return num1 / num2
-            else:
-                print(f"The num1 value you have entered is {num2}. This is results to maths error")
-        case _:
-            print("Invalid choice, recheck and try again")
-    
-    
-    
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            print("Cannot divide by zero")
+        else:
+            return num1 / num2

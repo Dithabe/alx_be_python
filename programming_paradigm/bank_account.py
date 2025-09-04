dc9cdd84780d84d9fd3cc509a8ac75c3bb596942
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-09-04 23:28:18 +0200

    Update bank_account.py
    removing extra characters on balance section

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -16,6 +16,5 @@ class BankAccount:
             return False
         
     def display_balance(self):
-        print('='*50)
         print(f"Current Balance: ${self.account_balance}")
-        print('='*50)
+       

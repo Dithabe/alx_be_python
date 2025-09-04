06337d177bc50c6bfd7514cdd73f976399706eb0
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-09-05 01:00:03 +0200

    Update robust_division_calculator.py
    updating the output text

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -7,8 +7,9 @@ def safe_divide(numerator, denominator):
         deno = float(denominator)
         if deno == 0:
             raise ZeroDivisionError("Error: Cannot divide by zero.")
-        return float(num / deno)
+        return f"The result of the division is {num/deno}"
 
     except ValueError:
         return "Error: Please enter numeric values only." 
+
         

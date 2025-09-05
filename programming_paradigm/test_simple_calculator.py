1a8c540e3f5e83070002abe1fca215ab5b9c7ec8
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-09-05 13:36:05 +0200

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,13 +11,14 @@ class testing(unittest.TestCase):
     def test_subtraction(self):   
         self.assertEqual(self.calc.subtract(15,5), 13) 
         
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(6,3), 2)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(8,2), 16)
 
     
+
 
 
 

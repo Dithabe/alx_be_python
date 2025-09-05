13a4db102179290bda4e1c0faa40f8cbf732497e
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-09-05 13:24:00 +0200

    Add files via upload

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,23 @@
+import unittest 
+from simple_calculator import SimpleCalculator
+
+class testing(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        result = SimpleCalculator.add(5,8)   
+        self.assertEqual(result, 13) 
+
+    def test_subtract(self):
+        result = SimpleCalculator.subtract(15,5)   
+        self.assertEqual(result, 13) 
+        
+    def test_divide(self):
+        result = SimpleCalculator.divide(6,3)
+        self.assertEqual(result, 2)
+
+    def tes_multiply(self):
+        result = SimpleCalculator.multiply(8,2)
+        self.assertEqual(result, 16)
+    
